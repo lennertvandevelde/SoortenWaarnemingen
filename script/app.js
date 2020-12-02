1bee0fffa306c40c2ebfca70526b4ea23258d18d
Lennert Van de Velde <lennert.van.de.velde@student.howest.be>
2020-12-02 22:02:40 +0100

    Heigth changed

--- script/app.js
+++ script/app.js
@@ -4,6 +4,18 @@ let graph;
 let plabels = [];
 let values = [];
 let first = true;
+let saves = [];
+const checkClicks = function () {
+  let adds = document.querySelectorAll('.js-add');
+  adds.forEach((add) => {
+    if (saves.includes(add.getAttribute('data-id'))) {
+      add.classList.add('c-clicked');
+      setTimeout(function () {
+        add.classList.add('o-hide-accessible');
+      }, 2000);
+    }
+  });
+};
 const addClicks = function () {
   let adds = document.querySelectorAll('.js-add');
   adds.forEach((add) => {
@@ -11,6 +23,7 @@ const addClicks = function () {
       save(add.getAttribute('data-id'));
     });
   });
+  checkClicks();
 };
 const removeprevious = function (classname) {
   let previous = document.querySelector('.js-back-list');
@@ -70,6 +83,7 @@ const updateChart = function () {
   });
 };
 const save = async (id) => {
+  saves.push(id);
   mainpage = document.querySelector('.js-main');
   console.log(id);
   const data = await fetch(`https://api.gbif.org/v1/species/${id}?language=nl`)
@@ -88,12 +102,11 @@ const save = async (id) => {
     let i = 0;
     let ImageElem = data2.results[i];
 
-    while (i < data2.results.length-1 && !ImageElem.media.length) {
+    while (i < data2.results.length - 1 && !ImageElem.media.length) {
       i += 1;
-	  ImageElem = data2.results[i];
+      ImageElem = data2.results[i];
+    }
 
-	}
-	
     if (ImageElem.media.length) {
       image = ImageElem.media[0].identifier;
     }
@@ -111,21 +124,55 @@ const save = async (id) => {
   }
   html += `</div>
 				<div class="c-card__body">
-					<img src="${image}" alt="Afbeelding van ${data.scientificName}" onerror="this.src='https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png'" style="width : 100%; height: 200px;  object-fit: cover;">
+					<img src="${image}" alt="Afbeelding van ${data.scientificName}" onerror="this.src='https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png'" style="width : 100%; height: 25vh;  object-fit: cover;">
 					<input class="o-hide-accessible c-option c-option--hidden js-checkbox" type="checkbox" id="checkbox${data.key}" data-name="${data.scientificName}">
-					<label class="c-label c-label--option c-custom-option js-show-label" data-id="${data.key}" data-count="${data3}" data-name="${data.scientificName}" data-checked=false for="checkbox${data.key}">
-						<span class="c-custom-option__fake-input c-custom-option__fake-input--checkbox">
-							<svg class="c-custom-option__symbol" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 6.75">
-								<path d="M4.75,9.5a1,1,0,0,1-.707-.293l-2.25-2.25A1,1,0,1,1,3.207,5.543L4.75,7.086,8.793,3.043a1,1,0,0,1,1.414,1.414l-4.75,4.75A1,1,0,0,1,4.75,9.5" transform="translate(-1.5 -2.75)"/>
-							</svg>
-						</span>
-						Toon op grafiek
-					</label>
+					<div class="c-card__buttons">
+					  <label class="c-label c-label--option c-custom-option js-show-label" data-id="${data.key}" data-count="${data3}" data-name="${data.scientificName}" data-checked=false for="checkbox${data.key}">
+  						<span class="c-custom-option__fake-input c-custom-option__fake-input--checkbox">
+  							<svg class="c-custom-option__symbol" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 6.75">
+  								<path d="M4.75,9.5a1,1,0,0,1-.707-.293l-2.25-2.25A1,1,0,1,1,3.207,5.543L4.75,7.086,8.793,3.043a1,1,0,0,1,1.414,1.414l-4.75,4.75A1,1,0,0,1,4.75,9.5" transform="translate(-1.5 -2.75)"/>
+  							</svg>
+  						</span>
+  						Toon op grafiek
+            </label>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="44" viewBox="0 0 24 48">
+  <g id="Group_10" data-name="Group 10" transform="translate(-530 1517.104)">
+    <g id="Rectangle_266" data-name="Rectangle 266" transform="translate(532 -1503.104)" fill="#fff" stroke="#707070" stroke-width="2">
+      <path d="M0,0H20a0,0,0,0,1,0,0V27a5,5,0,0,1-5,5H5a5,5,0,0,1-5-5V0A0,0,0,0,1,0,0Z" stroke="none"/>
+      <path d="M0-1H20a1,1,0,0,1,1,1V27a6,6,0,0,1-6,6H5a6,6,0,0,1-6-6V0A1,1,0,0,1,0-1Z" fill="none"/>
+    </g>
+    <g id="Union_1" data-name="Union 1" transform="translate(532 -1515.104)" fill="#fff">
+      <path d="M 20.99979972839355 8.000200271606445 L 19.99979972839355 8.000200271606445 L -1.358032193365943e-07 8.000200271606445 L -1.00000011920929 8.000200271606445 L -1.00000011920929 7.000200271606445 L -1.00000011920929 3.167100191116333 C -1.00000011920929 1.512510180473328 0.3456998765468597 0.166400134563446 1.999799847602844 0.166400134563446 L 7.749799728393555 0.166400134563446 L 7.749799728393555 1.358032193365943e-07 L 7.749799728393555 -0.9999998807907104 L 8.749799728393555 -0.9999998807907104 L 11.87459945678711 -0.9999998807907104 L 12.87459945678711 -0.9999998807907104 L 12.87459945678711 1.358032193365943e-07 L 12.87459945678711 0.166400134563446 L 18 0.166400134563446 C 19.65410041809082 0.166400134563446 20.99979972839355 1.512510180473328 20.99979972839355 3.167100191116333 L 20.99979972839355 7.000200271606445 L 20.99979972839355 8.000200271606445 Z" stroke="none"/>
+      <path d="M 19.99979972839355 7.000200271606445 L 19.99979972839355 3.167100191116333 C 19.99979972839355 2.06190013885498 19.10429954528809 1.166400194168091 18 1.166400194168091 L 11.87459945678711 1.166400194168091 L 11.87459945678711 1.358032193365943e-07 L 8.749799728393555 1.358032193365943e-07 L 8.749799728393555 1.166400194168091 L 1.999799847602844 1.166400194168091 C 0.8954998850822449 1.166400194168091 -1.358032193365943e-07 2.06190013885498 -1.358032193365943e-07 3.167100191116333 L -1.358032193365943e-07 7.000200271606445 L 19.99979972839355 7.000200271606445 M 21.99979972839355 9.000200271606445 L 19.99979972839355 9.000200271606445 L -1.358032193365943e-07 9.000200271606445 L -2.000000238418579 9.000200271606445 L -2.000000238418579 7.000200271606445 L -2.000000238418579 3.167100191116333 C -2.000000238418579 0.9611101150512695 -0.2057001292705536 -0.833599865436554 1.999799847602844 -0.833599865436554 L 6.749799728393555 -0.833599865436554 L 6.749799728393555 -1.99999988079071 L 8.749799728393555 -1.99999988079071 L 11.87459945678711 -1.99999988079071 L 13.87459945678711 -1.99999988079071 L 13.87459945678711 -0.833599865436554 L 18 -0.833599865436554 C 20.20549964904785 -0.833599865436554 21.99979972839355 0.9611101150512695 21.99979972839355 3.167100191116333 L 21.99979972839355 7.000200271606445 L 21.99979972839355 9.000200271606445 Z" stroke="none" fill="#707070"/>
+    </g>
+    <g id="Rectangle_269" data-name="Rectangle 269" transform="translate(535 -1497.104)" fill="#fff" stroke="#707070" stroke-width="1">
+      <rect width="2" height="22" stroke="none"/>
+      <rect x="0.5" y="0.5" width="1" height="21" fill="none"/>
+    </g>
+    <g id="Rectangle_270" data-name="Rectangle 270" transform="translate(539 -1495.104)" fill="#fff" stroke="#707070" stroke-width="1">
+      <rect width="2" height="19" stroke="none"/>
+      <rect x="0.5" y="0.5" width="1" height="18" fill="none"/>
+    </g>
+    <g id="Rectangle_272" data-name="Rectangle 272" transform="translate(543 -1495.104)" fill="#fff" stroke="#707070" stroke-width="1">
+      <rect width="2" height="19" stroke="none"/>
+      <rect x="0.5" y="0.5" width="1" height="18" fill="none"/>
+    </g>
+    <g id="Rectangle_271" data-name="Rectangle 271" transform="translate(547 -1497.104)" fill="#fff" stroke="#707070" stroke-width="1">
+      <rect width="2" height="22" stroke="none"/>
+      <rect x="0.5" y="0.5" width="1" height="21" fill="none"/>
+    </g>
+  </g>
+</svg>
+
+
+
+					</div>
 				</div>
 			</div>
 		</div>`;
 
   mainpage.innerHTML += html;
+  checkClicks();
   graph = document.querySelector('.js-bar');
   updateChart();
   let checkboxes = document.querySelectorAll('.js-checkbox');
@@ -157,6 +204,7 @@ const save = async (id) => {
       }
     });
   });
+
   updateChart();
 };
 const showSpecies = async (genus) => {
@@ -180,12 +228,10 @@ const showSpecies = async (genus) => {
 			  <span class="c-main-nav__link js-order"data-id=${result.key} >
 				  ${result.scientificName} (${result.rank})
 			  </span>
-			  <svg class="js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
-			  <g id="Group_9" data-name="Group 9" transform="translate(-136 -368)">
-				  <line id="Line_14" data-name="Line 14" y2="64" transform="translate(168 368)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-				  <line id="Line_15" data-name="Line 15" x2="64" transform="translate(136 400)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-			  </g>
-			  </svg>
+			  <svg class="c-add js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
+			  <line class="c-add__ver" id="Line_14" data-name="Line 14" y2="64" transform="translate(32 0)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+			  <line class="c-add__hor" id="Line_15" data-name="Line 15" x2="64" transform="translate(0 32)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+		  </svg>
 		  </li>`;
     }
     console.log(data.results.length);
@@ -201,7 +247,7 @@ const showSpecies = async (genus) => {
     console.log(data);
   }
   sidebar.innerHTML = html;
-  addClicks()
+  addClicks();
 };
 const showGenus = async (family) => {
   // Eerst bouwen we onze url op
@@ -224,12 +270,10 @@ const showGenus = async (family) => {
 			<span class="c-main-nav__link js-genus"data-id=${result.key} data-name=${result.scientificName} data-rank=${result.rank}>
 				${result.scientificName} (${result.rank})
 			</span>
-			<svg class="js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
-			<g id="Group_9" data-name="Group 9" transform="translate(-136 -368)">
-				<line id="Line_14" data-name="Line 14" y2="64" transform="translate(168 368)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-				<line id="Line_15" data-name="Line 15" x2="64" transform="translate(136 400)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-			</g>
-			</svg>
+			<svg class="c-add js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
+			  <line class="c-add__ver" id="Line_14" data-name="Line 14" y2="64" transform="translate(32 0)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+			  <line class="c-add__hor" id="Line_15" data-name="Line 15" x2="64" transform="translate(0 32)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+		  </svg>
 		</li>`;
     }
     console.log(data.results.length);
@@ -254,7 +298,7 @@ const showGenus = async (family) => {
       showSpecies(genus);
     });
   });
-  addClicks()
+  addClicks();
 };
 const showFamily = async (order) => {
   // Eerst bouwen we onze url op
@@ -272,11 +316,9 @@ const showFamily = async (order) => {
 		  <span class="c-main-nav__link js-family"data-id=${result.key} data-name=${result.scientificName} data-rank=${result.rank}>
 			  ${result.scientificName} (${result.rank})
 		  </span>
-		  <svg class="js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
-		  <g id="Group_9" data-name="Group 9" transform="translate(-136 -368)">
-			  <line id="Line_14" data-name="Line 14" y2="64" transform="translate(168 368)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-			  <line id="Line_15" data-name="Line 15" x2="64" transform="translate(136 400)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-		  </g>
+		  <svg class="c-add js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
+			  <line class="c-add__ver" id="Line_14" data-name="Line 14" y2="64" transform="translate(32 0)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+			  <line class="c-add__hor" id="Line_15" data-name="Line 15" x2="64" transform="translate(0 32)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
 		  </svg>
 	  </li>`;
   }
@@ -291,7 +333,7 @@ const showFamily = async (order) => {
       showGenus(family);
     });
   });
-  addClicks()
+  addClicks();
 };
 
 const showOrder = async (pclass) => {
@@ -310,11 +352,9 @@ const showOrder = async (pclass) => {
 		  <span class="c-main-nav__link js-order"data-id=${result.key} data-name=${result.scientificName} data-rank=${result.rank}>
 			  ${result.scientificName} (${result.rank})
 		  </span>
-		  <svg class="js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
-		  <g id="Group_9" data-name="Group 9" transform="translate(-136 -368)">
-			  <line id="Line_14" data-name="Line 14" y2="64" transform="translate(168 368)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-			  <line id="Line_15" data-name="Line 15" x2="64" transform="translate(136 400)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-		  </g>
+		  <svg class="c-add js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
+			  <line class="c-add__ver" id="Line_14" data-name="Line 14" y2="64" transform="translate(32 0)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+			  <line class="c-add__hor" id="Line_15" data-name="Line 15" x2="64" transform="translate(0 32)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
 		  </svg>
 	  </li>`;
   }
@@ -330,7 +370,7 @@ const showOrder = async (pclass) => {
       showFamily(order);
     });
   });
-  addClicks()
+  addClicks();
 };
 const showClass = async (phyl) => {
   // Eerst bouwen we onze url op
@@ -348,11 +388,9 @@ const showClass = async (phyl) => {
 		  <span class="c-main-nav__link js-class"data-id=${result.key} data-name=${result.scientificName} data-rank=${result.rank}>
 			  ${result.scientificName} (${result.rank})
 		  </span>
-		  <svg class="js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
-		  <g id="Group_9" data-name="Group 9" transform="translate(-136 -368)">
-			  <line id="Line_14" data-name="Line 14" y2="64" transform="translate(168 368)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-			  <line id="Line_15" data-name="Line 15" x2="64" transform="translate(136 400)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-		  </g>
+		  <svg class="c-add js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
+			  <line class="c-add__ver" id="Line_14" data-name="Line 14" y2="64" transform="translate(32 0)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+			  <line class="c-add__hor" id="Line_15" data-name="Line 15" x2="64" transform="translate(0 32)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
 		  </svg>
 	  </li>`;
   }
@@ -369,7 +407,7 @@ const showClass = async (phyl) => {
       showOrder(pclass);
     });
   });
-  addClicks()
+  addClicks();
 };
 
 const showSub = async (kingdom) => {
@@ -388,12 +426,10 @@ const showSub = async (kingdom) => {
 		<span class="c-main-nav__link js-sub"data-id=${result.key} data-name=${result.scientificName} data-rank=${result.rank}>
 			${result.scientificName} (${result.rank})
 		</span>
-		<svg class="js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
-		<g id="Group_9" data-name="Group 9" transform="translate(-136 -368)">
-			<line id="Line_14" data-name="Line 14" y2="64" transform="translate(168 368)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-			<line id="Line_15" data-name="Line 15" x2="64" transform="translate(136 400)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-		</g>
-		</svg>
+		<svg class="c-add js-add" data-id=${result.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
+			  <line class="c-add__ver" id="Line_14" data-name="Line 14" y2="64" transform="translate(32 0)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+			  <line class="c-add__hor" id="Line_15" data-name="Line 15" x2="64" transform="translate(0 32)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+		  </svg>
 	</li>`;
   }
   sidebar.innerHTML = html;
@@ -423,7 +459,7 @@ const showSub = async (kingdom) => {
     });
   }
 
-  addClicks()
+  addClicks();
 };
 
 const getKingdomNames = async () => {
@@ -439,12 +475,10 @@ const getKingdomNames = async () => {
 				<span class="c-main-nav__link js-kingdom" data-id=${element.key} data-name=${element.scientificName} data-rank=${element.rank}>
 					${element.scientificName} (${element.rank})
 				</span>
-				<svg class="js-add" data-id=${element.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64" data-id=${element.key}>
-				<g id="Group_9" data-name="Group 9" transform="translate(-136 -368)">
-					<line id="Line_14" data-name="Line 14" y2="64" transform="translate(168 368)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-					<line id="Line_15" data-name="Line 15" x2="64" transform="translate(136 400)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
-				</g>
-				</svg>
+        <svg class="c-add js-add" data-id=${element.key} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 64 64">
+			  <line class="c-add__ver" id="Line_14" data-name="Line 14" y2="64" transform="translate(32 0)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+			  <line class="c-add__hor" id="Line_15" data-name="Line 15" x2="64" transform="translate(0 32)" fill="none" stroke="#f9f8f6" stroke-width="5"/>
+		  </svg>
 
 			</li>`;
   });
@@ -478,7 +512,7 @@ const getKingdomNames = async () => {
     });
   });
 
-  addClicks()
+  addClicks();
   first = false;
 };
 
